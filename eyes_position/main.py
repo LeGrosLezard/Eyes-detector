56d0c14757f972eb3ed7ab829040d91ce8ef4164
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-09-04 18:39:33 +0200

    Update and rename main_yeux.py to main.py

--- eyes_position/main.py
+++ eyes_position/main.py
@@ -3,10 +3,16 @@ import cv2
 from PIL import Image
 import os
 
-from traitement_image import *
+from traitement_image import pre_initialisation
+from traitement_image import position_yeux_verticale
+from traitement_image import qualibrage
+from traitement_image import position_yeux_horizontal
+from traitement_image import association
+from traitement_image import pre_initialisation
 
 
 def video_capture_yeux():
+    """Here we lunch the video display (pc cam) with VideoCapture"""
 
     LISTE_AJUSTEMENT = []
     LISTE_DROITE_GAUCHE = []
@@ -16,15 +22,14 @@ def video_capture_yeux():
 
     while(True):
 
-
         left_eye = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
-        
-          
+
+
         ret, frame = video.read()
         frame = cv2.resize(frame, (600, 600))
         eyes = left_eye.detectMultiScale(frame)
 
-        
+
         if len(LISTE_AJUSTEMENT) < 50:
             pre_initialisation(eyes, LISTE_AJUSTEMENT, frame)
             print("initialisation")
@@ -35,26 +40,25 @@ def video_capture_yeux():
 
             association(position1, position2, LISTE_AJUSTEMENT)
 
-            #---------------------------------------------------Partie effacage de liste
-            #permet d'effacer la liste et de refaire une initialisation
+
             LISTE_QUALIBRAGE.append(position1)
             qualibration = qualibrage(LISTE_QUALIBRAGE)
             if qualibration == "qualibration":
                 LISTE_AJUSTEMENT = []
 
-            if position1 in ("le mec s'est baissé", "le mec s'est levé",
-                             "le mec à levé la tete", "le mec à baisser la tete"):
+            if position1 in ("lthe person bent down",
+                             "the person got up",
+                             "the person lifted his head",
+                             "the person dropped his head"):
+
                 LISTE_AJUSTEMENT = []
 
 
-
-
-        cv2.imshow('YEUX CAPTURE', frame)
+        cv2.imshow('EYES CAPTURE', frame)
 
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
 
     video.release()
     cv2.destroyAllWindows()
@@ -66,15 +70,3 @@ def video_capture_yeux():
 
 if __name__ == "__main__":
     video_capture_yeux()
-
-
-
-
-
-
-
-
-
-
-
-
