e4df123b19a97c4e4db80241e26b199b0bedac7d
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-09-04 18:50:37 +0200

    Update main.py
    translation

--- eyes_position/main.py
+++ eyes_position/main.py
@@ -14,44 +14,56 @@ from traitement_image import pre_initialisation
 def video_capture_yeux():
     """Here we lunch the video display (pc cam) with VideoCapture"""
 
-    LISTE_AJUSTEMENT = []
-    LISTE_DROITE_GAUCHE = []
-    LISTE_QUALIBRAGE = []
+    #We initialize list
+    FIT_LIST = []
+    LIST_RIGHT_LEFT = []
+    QUALIFER_LIST = []
 
     video = cv2.VideoCapture(0)
 
     while(True):
 
+        #Eyes haarcascade
         left_eye = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
 
-
+        #We define the current frame and resize it
         ret, frame = video.read()
         frame = cv2.resize(frame, (600, 600))
+
+        #We try to detect eyes into the frame 
         eyes = left_eye.detectMultiScale(frame)
 
-
-        if len(LISTE_AJUSTEMENT) < 50:
-            pre_initialisation(eyes, LISTE_AJUSTEMENT, frame)
+        #we fill in our list of the current position
+        #of the eyes in order to make an average of
+        #them and then to be able to continue them.
+        if len(FIT_LIST) < 50:
+            pre_initialisation(eyes, FIT_LIST, frame)
             print("initialisation")
 
+        #Now we catch the current position and
+        #if the eyes move by -5 pixels for example
+        #(on the y-axis) it is because the person looked up.
         else:
-            position1 = position_yeux_verticale(eyes, LISTE_AJUSTEMENT, frame)
-            position2 = position_yeux_horizontal(eyes, LISTE_DROITE_GAUCHE, frame)
+            position1 = position_yeux_verticale(eyes, FIT_LIST, frame)
+            position2 = position_yeux_horizontal(eyes, LIST_RIGHT_LEFT, frame)
+            #
+            association(position1, position2, FIT_LIST)
 
-            association(position1, position2, LISTE_AJUSTEMENT)
 
-
-            LISTE_QUALIBRAGE.append(position1)
-            qualibration = qualibrage(LISTE_QUALIBRAGE)
+            QUALIFER_LIST.append(position1)
+            qualibration = qualibrage(QUALIFER_LIST)
             if qualibration == "qualibration":
-                LISTE_AJUSTEMENT = []
+                FIT_LIST = []
 
-            if position1 in ("lthe person bent down",
+
+            #If the person bent down, got up ...
+            #We empty the list to re-initialize
+            if position1 in ("the person bent down",
                              "the person got up",
                              "the person lifted his head",
                              "the person dropped his head"):
 
-                LISTE_AJUSTEMENT = []
+                FIT_LIST = []
 
 
         cv2.imshow('EYES CAPTURE', frame)
